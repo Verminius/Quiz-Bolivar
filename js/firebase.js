eac80202cd7cd914c278e84eff91caea7cce0ca2
Alexander Patiño <alpatino.ortiz@gmail.com>
2023-06-06 17:27:32 -0500

    creando base de datos en firebase

---
+++ js/firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
+import {} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYdnPOTC7G0KeuemJ3PiIFyULXwFUR9xA",
+
+  authDomain: "quiz-bolivar.firebaseapp.com",
+
+  projectId: "quiz-bolivar",
+
+  storageBucket: "quiz-bolivar.appspot.com",
+
+  messagingSenderId: "1044902982305",
+
+  appId: "1:1044902982305:web:9f6d126c7e31c5b871b6e0",
+
+  measurementId: "G-DZKWQ76Y64",
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
--- js/highscores.js
+++ js/highscores.js
@@ -1,8 +1,1 @@
 const highScoresList = document.querySelector("#highScoreList");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-highScoresList.innerHTML = highScores
-  .map((score) => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
