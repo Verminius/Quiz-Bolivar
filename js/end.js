3c028a940fc5fa43e335cd4a0411f375cb5ef672
Alexander Patiño <alpatino.ortiz@gmail.com>
2023-06-07 03:02:04 -0500

    configurando firebase

--- js/end.js
+++ js/end.js
@@ -1,9 +1,25 @@
-window.addEventListener("DOMContentLoaded", () => {});
+import { saveTask } from "./firebase.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+
+});
+
 
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 const finalScore = document.querySelector("#finalScore");
 
-finalScore.innerText = mostRecentScore;
+finalScore.value = mostRecentScore;
 
-const username = document.querySelector("#username");
-const saveScoreBtn = document.querySelector("#saveScoreBtn");
+// const username = document.querySelector("#username");
+// const saveScoreBtn = document.querySelector("#saveScoreBtn");
+
+const taskForm = document.getElementById('task-form');
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const score = taskForm['finalScore']
+    const name = taskForm['username']
+
+    saveTask(name.value, score.value)
+
+})
--- js/firebase.js
+++ js/firebase.js
@@ -2,30 +2,20 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-analytics.js";
-import {} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
-
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDYdnPOTC7G0KeuemJ3PiIFyULXwFUR9xA",
-
-  authDomain: "quiz-bolivar.firebaseapp.com",
-
-  projectId: "quiz-bolivar",
-
-  storageBucket: "quiz-bolivar.appspot.com",
-
-  messagingSenderId: "1044902982305",
-
-  appId: "1:1044902982305:web:9f6d126c7e31c5b871b6e0",
-
-  measurementId: "G-DZKWQ76Y64",
+    apiKey: "AIzaSyDYdnPOTC7G0KeuemJ3PiIFyULXwFUR9xA",
+    authDomain: "quiz-bolivar.firebaseapp.com",
+    projectId: "quiz-bolivar",
+    storageBucket: "quiz-bolivar.appspot.com",
+    messagingSenderId: "1044902982305",
+    appId: "1:1044902982305:web:9f6d126c7e31c5b871b6e0",
+    measurementId: "G-DZKWQ76Y64"
 };
 
 // Initialize Firebase
@@ -33,3 +23,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const analytics = getAnalytics(app);
+
+const db = getFirestore(app)
+
+export const saveTask = (name, score) =>
+    addDoc(collection(db, 'scores'), { name, score })
